65fab41ce44a3a0d1fd24b8e7946bb03cd9ac416
abdo20920al-spec <abdo20920al@gmail.com>
2026-07-13 04:12:53 +0200

    تعديل lib/main.dart - إضافة Firebase و Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'screens/navigation_screen.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const RexApp());
 }
+
 
 class RexApp extends StatelessWidget {
   const RexApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'REX',
+      title: "REX",
+
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.deepPurple,
-        scaffoldBackgroundColor: Colors.black,
       ),
+
       home: const NavigationScreen(),
     );
   }
